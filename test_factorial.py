d3ebab4de8283c13de4d5d3527a65a8c168aa640
shivanivsdeveloper <shivanivs202007@gmail.com>
2026-09-23 08:51:11 +0530

    Initial commit

---
+++ test_factorial.py
@@ -0,0 +1,30 @@
+import pytest
+from factorial import factorial
+def test_factorial(zero):
+    assert factorial(0) == 1
+def test_factorial(one):
+    assert factorial(1) == 1
+def test_factorial(five):
+    assert factorial(5) == 120
+def test_factorial(ten):
+    assert factorial(10) == 3628800
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
